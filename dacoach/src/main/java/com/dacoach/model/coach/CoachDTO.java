6688a2a3579965c68054748a501b71cb854d504c
jaein <jaein365@naver.com>
2026-02-02 19:09:42 +0900

    feat:dto작성중

--- dacoach/src/main/java/com/dacoach/model/coach/CoachDTO.java
+++ dacoach/src/main/java/com/dacoach/model/coach/CoachDTO.java
@@ -1,5 +1,20 @@
 package com.dacoach.model.coach;
 
-public class CoachDTO {
+import java.sql.Date;
 
+import com.dacoach.model.users.UsersDTO;
+
+public class CoachDTO {
+	private int coachIdx;
+	private int usersIdx;
+	private Date birthDate;
+	private String nickName;
+	private String phone;
+	private String mail;
+	private String intro;
+	private String photo;
+	private String video;
+	
+	private UsersDTO usersDto;
+	
 }
