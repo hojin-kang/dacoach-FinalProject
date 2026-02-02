ea9f5a5f24cf60418b920837779811eecac45ed4
jaein <jaein365@naver.com>
2026-02-02 17:32:04 +0900

    feat:id중복체크 구현중

---
+++ dacoach/src/main/java/com/dacoach/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.dacoach.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class UserController {
+	@GetMapping("/login")
+	public String login() {
+		return "users/login";
+	}
+	@GetMapping("/userType")
+	public String userType() {
+		return "users/userType";
+	}
+}
