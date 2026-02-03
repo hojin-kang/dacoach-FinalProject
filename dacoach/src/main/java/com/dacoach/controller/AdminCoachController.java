887e5d64ab34bd04f4ebe59a6efcb439b0ed3cf6
hyeonbin <binnara5@naver.com>
2026-02-03 12:33:29 +0900

    코치리스트 자격증 업데이트

--- dacoach/src/main/java/com/dacoach/controller/AdminCoachController.java
+++ dacoach/src/main/java/com/dacoach/controller/AdminCoachController.java
@@ -1,21 +1,43 @@
 package com.dacoach.controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dacoach.service.admin.AdminService;
 
 @Controller
 @RequestMapping("/admin/coach")
 public class AdminCoachController {
 
+	@Autowired
+	private AdminService adminService;
+	
 	
 	@GetMapping("/coachList")
-    public String coachList(Model model) {
-        model.addAttribute("contentPage", "admin/coach/coachList");
-        model.addAttribute("contentFragment", "coachContent");
-        
-        //model.addAttribute("userList", coachService.getUserList());
-        return "admin/dashboard";
-    }
+	public String coachList(Model model) {
+	    List<Map<String, Object>> coachList = new ArrayList<>();
+	    int certCount = 0;
+	    
+	    try {
+	        coachList = adminService.getCoachList();
+	        certCount = adminService.getCertCount();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    model.addAttribute("coachList", coachList);
+	    model.addAttribute("certCount", certCount);
+	    
+	    model.addAttribute("contentPage", "admin/coach/coachList");
+	    model.addAttribute("contentFragment", "coachContent");
+	    
+	    return "admin/dashboard";
+	}
+	
 }
---
+++ dacoach/src/main/java/com/dacoach/service/admin/AdminServiceImple.java
@@ -0,0 +1,27 @@
+package com.dacoach.service.admin;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dacoach.mapper.admin.AdminMapper;
+
+@Service
+public class AdminServiceImple implements AdminService {
+
+	@Autowired
+	private AdminMapper adminMapper;
+	
+	@Override
+	public List<Map<String, Object>> getCoachList() throws Exception {
+		return adminMapper.getCoachList();
+	}
+	
+	@Override
+	public int getCertCount() throws Exception {
+		return adminMapper.getCertCount();
+	}
+
+}
