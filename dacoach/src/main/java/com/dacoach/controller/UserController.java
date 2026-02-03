57b93332b03eb58192d794f2695ea24e75a67cf9
jaein <jaein365@naver.com>
2026-02-03 11:32:02 +0900

    feat:헤더 바뀜, 로그아웃 완료

--- dacoach/src/main/java/com/dacoach/controller/UserController.java
+++ dacoach/src/main/java/com/dacoach/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,7 +20,11 @@ public class UserController {
 	private UsersService usersService;
 	@Autowired
 	private CoachService coachService;
-	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
 	@GetMapping("/login")
 	public String login() {
 		return "users/login";
