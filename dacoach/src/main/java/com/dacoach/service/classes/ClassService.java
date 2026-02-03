4e883e4391b3bf85062a53fad03ef77d36c89e3a
GUI_jinyu <ybhhyo08@naver.com>
2026-02-03 17:33:32 +0900

    클래스 등록

--- dacoach/src/main/java/com/dacoach/service/classes/ClassService.java
+++ dacoach/src/main/java/com/dacoach/service/classes/ClassService.java
@@ -1,7 +1,18 @@
 package com.dacoach.service.classes;
 
+import java.util.List;
+import java.util.Map;
 import com.dacoach.model.classes.ClassDTO;
 
 public interface ClassService {
+
 	int classRegister(ClassDTO classDTO);
+
+	List<Map<String, Object>> getMajorFields();
+
+	List<Map<String, Object>> getMinorFields(Integer majorFieldIdx);
+
+	List<Map<String, Object>> getMajorRegions();
+
+	List<Map<String, Object>> getMinorRegions(Integer majorRegionIdx);
 }
