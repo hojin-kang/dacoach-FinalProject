563d4b94c1c9ef8ebbaf41118f8c62c805b214ef
hojin-kang <ghwls7283@naver.com>
2026-02-04 18:08:09 +0900

    퇴근핑

--- dacoach/src/main/java/com/dacoach/controller/CompanyController.java
+++ dacoach/src/main/java/com/dacoach/controller/CompanyController.java
@@ -6,13 +6,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dacoach.model.users.UsersDTO;
 import com.dacoach.service.company.CompanyService;
 
 @Controller
@@ -32,33 +39,47 @@ public class CompanyController {
 
 		return "/company/join/companyJoin";
 	}
-	@GetMapping("/company/profile/companyInfo")
-	public ModelAndView companyInfo(String login_id,
-									String password,
-									String user_name,
-									String user_type,
-									String status){
-		m.put("login_id", login_id);
-		m.put("password", password);
-		m.put("user_name", user_name);
-		m.put("user_type", user_type);
-		m.put("status", status);
-
+	
+	@PostMapping("company/join/loginInfoOK")
+	public ModelAndView LoginInfoOk(UsersDTO dto) {
+		try {
+			int idx = companyService.joinOk(dto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("login_id",dto.getLogin_id());
+		mav.setViewName("forward:/company/profile/companyInfo");
+		return mav;
+	}
+	
+	@RequestMapping("/company/profile/companyInfo")
+	public ModelAndView companyInfo() {
 		
 		ModelAndView mav=new ModelAndView();
+		int idx=0;
+		try {
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		mav.setViewName("/company/profile/companyInfo");
 		return mav;
 	}
 	
-	@GetMapping("/company/profile/profileForm")
+	@PostMapping("/company/profile/profileForm")
+	@ResponseBody()
 	public ModelAndView profileForm(String phone,
 									String address,
 									String email,
-									String company_num,
+									Integer company_num,
 									String cert_status,
 									String cert_name,
-									String cert_file) {
+									String cert_file,
+									String login_id) {
 		m.put("phone", phone);
 		m.put("address",address);
 		m.put("email", email);
@@ -66,7 +87,20 @@ public class CompanyController {
 		m.put("cert_status", cert_status);
 		m.put("cert_name", cert_name);
 		m.put("cert_file", cert_file);
-		System.out.println(m);
+		System.out.println(login_id);
+		try {
+			int user_idx=companyService.getUserIdx(login_id);
+			m.put("user_idx", user_idx);
+			System.out.println(m);
+			int infoResult=companyService.companyInfo(m);
+			int certResurt=companyService.insertcert(m);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		ModelAndView mav=new ModelAndView();
 		List<Map<String,Object>> field=null;
 		List<Map<String,Object>> region=null;
@@ -81,4 +115,20 @@ public class CompanyController {
 		mav.setViewName("/company/profile/profileForm");
 		return mav;
 	}
+	
+	
+	@GetMapping("/api/company/getMinorRegion")
+	public ResponseEntity<List<Map<String, Object>>> tst(Integer regionIdx) {
+		List<Map<String, Object>> minorRegion=null;
+		try {
+			minorRegion = companyService.getRegionTag(regionIdx);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ResponseEntity<List<Map<String, Object>>> re=new ResponseEntity<List<Map<String, Object>>>(minorRegion,HttpStatus.OK);
+		return re;
+	}
+
+	
 }
--- dacoach/src/main/java/com/dacoach/service/company/CompanyServiceImple.java
+++ dacoach/src/main/java/com/dacoach/service/company/CompanyServiceImple.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dacoach.mapper.company.CompanyMapper;
+import com.dacoach.model.users.UsersDTO;
 
 @Service
 public class CompanyServiceImple implements CompanyService {
@@ -23,5 +24,30 @@ public class CompanyServiceImple implements CompanyService {
 		
 		return companyMapper.regionTag();
 	}
+	@Override
+	public List<Map<String, Object>> getRegionTag(int idx) throws Exception {
+		
+		return companyMapper.getRegionTag(idx);
+	}
+	
+	@Override
+	public int joinOk(UsersDTO dto) throws Exception {
 
+		int result=companyMapper.joinOk(dto);
+		return  result;
+
+	}
+	@Override
+	public int getUserIdx(String login_id) throws Exception {
+		
+		return companyMapper.getUserIdx(login_id);
+	}
+	@Override
+	public int companyInfo(Map<String, Object> map) throws Exception {
+		
+		return companyMapper.companyInfo(map);
+	}
+	public int insertcert(Map<String, Object> map) throws Exception{
+		return companyMapper.insertcert(map);
+	}
 }
