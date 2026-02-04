7c3b317ad43650f5cdcb82f3770f49703ac3ac0a
GUI_jinyu <ybhhyo08@naver.com>
2026-02-04 12:49:20 +0900

    add throws exception

--- dacoach/src/main/java/com/dacoach/mapper/classes/ClassMapper.java
+++ dacoach/src/main/java/com/dacoach/mapper/classes/ClassMapper.java
@@ -8,24 +8,24 @@ import com.dacoach.model.classes.ClassDTO;
 public interface ClassMapper {
 
 	// 클래스 등록
-	int insertClass(ClassDTO classDTO);
+	int insertClass(ClassDTO classDTO) throws Exception;
 
 	// 대분류 조회 (Ajax용)
-	List<Map<String, Object>> selectMajorFields();
+	List<Map<String, Object>> selectMajorFields() throws Exception;
 
 	// 소분류 조회 (대분류 선택시 Ajax)
-	List<Map<String, Object>> selectMinorFields(Integer major_field_idx);
+	List<Map<String, Object>> selectMinorFields(Integer major_field_idx) throws Exception;
 
 	// 대지역 조회 (Ajax용)
-	List<Map<String, Object>> selectMajorRegions();
+	List<Map<String, Object>> selectMajorRegions() throws Exception;
 
 	// 소지역 조회 (대지역 선택시 Ajax)
-	List<Map<String, Object>> selectMinorRegions(Integer major_region_idx);
+	List<Map<String, Object>> selectMinorRegions(Integer major_region_idx) throws Exception;
 	
 	// 클래스 조회
-	List<ClassDTO> selectClassesByProvider(Integer provider_idx);
+	List<ClassDTO> selectClassesByProvider(Integer provider_idx) throws Exception;
 
 	// 코치 - 클래스 검색
-	List<ClassDTO> searchCoachClasses(Map<String, Object> param);
+	List<ClassDTO> searchCoachClasses(Map<String, Object> param) throws Exception;
 
 }
