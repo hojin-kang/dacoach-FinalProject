77c2a5e1cabdb4be333fd02514af00db99f86081
hyeonbin <binnara5@naver.com>
2026-02-04 19:39:10 +0900

    정지 상태 업데이트

--- dacoach/src/main/java/com/dacoach/service/admin/AdminService.java
+++ dacoach/src/main/java/com/dacoach/service/admin/AdminService.java
@@ -2,9 +2,15 @@ package com.dacoach.service.admin;
 
 import java.util.*;
 
+import com.dacoach.model.admin.EmbeddedUserDTO;
+
 
 public interface AdminService {
 	List<Map<String,Object>> getCoachList() throws Exception;
 	public int getCertCount() throws Exception;
 	List<Map<String,Object>> getCoachDetail(int coachidx) throws Exception;
+	public String getCoachStatus(int userIdx) throws Exception;
+	public EmbeddedUserDTO getCoachEmbedded(int userIdx) throws Exception;
+	public int updateCoachSuspended(int userIdx) throws Exception;
+	public int insertCoachSuspended(EmbeddedUserDTO dto) throws Exception;
 }
--- dacoach/src/main/java/com/dacoach/service/admin/AdminServiceImple.java
+++ dacoach/src/main/java/com/dacoach/service/admin/AdminServiceImple.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dacoach.mapper.admin.AdminMapper;
+import com.dacoach.model.admin.EmbeddedUserDTO;
 
 @Service
 public class AdminServiceImple implements AdminService {
@@ -27,6 +28,26 @@ public class AdminServiceImple implements AdminService {
 	@Override
 	public List<Map<String, Object>> getCoachDetail(int coachidx) throws Exception {
 		return adminMapper.getCoachDetail(coachidx);
+	}
+	
+	@Override
+	public String getCoachStatus(int userIdx) throws Exception {
+		return adminMapper.getCoachStatus(userIdx);
+	}
+
+	@Override
+	public EmbeddedUserDTO getCoachEmbedded(int userIdx) throws Exception {
+		return adminMapper.getCoachEmbedded(userIdx);
+	}
+	
+	@Override
+	public int updateCoachSuspended(int userIdx) throws Exception {
+		return adminMapper.updateCoachSuspended(userIdx);
+	}
+	
+	@Override
+	public int insertCoachSuspended(EmbeddedUserDTO dto) throws Exception {
+		return adminMapper.insertCoachSuspended(dto);
 	}
 
 }
