778e6dd7301e1af5411d2bb0dcd11e306e7ed1f3
wonjae <staregreg01@naver.com>
2026-02-06 02:14:36 +0900

    companyDTO int

--- dacoach/src/main/java/com/dacoach/model/company/CompanyDTO.java
+++ dacoach/src/main/java/com/dacoach/model/company/CompanyDTO.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 public class CompanyDTO {
  private int company_idx;
  private int users_idx;
- private String company_num;
+ private int company_num;
  private String phone;
  private String address;
  private String intro;
