984554a0e315b1908d313c1fc72388d70a8b2864
uoehoswi3 <thdus1821@naver.com>
2026-02-12 15:59:49 +0900

    회원탈퇴 기능 만드는중...&토큰내역 수정

--- dacoach/src/main/java/com/dacoach/controller/CoachSearchController.java
+++ dacoach/src/main/java/com/dacoach/controller/CoachSearchController.java
@@ -178,7 +178,7 @@ public class CoachSearchController {
 	 	        coachSearchService.useTokens(me, type.equals("CHAT") ? 1 : 3);
 	 	        
 	 	        // 4. token_history 데이터 삽입
-	 	        coachSearchService.addTokenHistory(me, type.equals("CHAT")?"채팅 신청":"매칭 신청", type.equals("CHAT")?1:3, type.equals("CHAT")?mytoken-1:mytoken-3);
+	 	        coachSearchService.addTokenHistory(me, type.equals("CHAT")?"채팅 신청":"매칭 신청", type.equals("CHAT")?-1:-3, type.equals("CHAT")?mytoken-1:mytoken-3);
 	 	        result.put("status", "success");
 
 	 	    } catch (Exception e) {
--- dacoach/src/main/java/com/dacoach/service/mypage/MypageService.java
+++ dacoach/src/main/java/com/dacoach/service/mypage/MypageService.java
@@ -5,4 +5,7 @@ import java.util.*;
 public interface MypageService {
 
 	public Map getUserInfo(int user_idx);
+	
+	List<Map<String,Object>> getWithdrawReasons();
+	void withdrawUser(int userIdx, int reasonTypeIdx, String extra);
 }
