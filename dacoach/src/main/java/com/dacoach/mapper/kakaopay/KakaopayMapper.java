44f875594fb03874fd00c6cf51e18c04b2fc9be2
hojin-kang <ghwls7283@naver.com>
2026-02-13 17:52:54 +0900

    퇴근

--- dacoach/src/main/java/com/dacoach/mapper/kakaopay/KakaopayMapper.java
+++ dacoach/src/main/java/com/dacoach/mapper/kakaopay/KakaopayMapper.java
@@ -3,6 +3,8 @@ package com.dacoach.mapper.kakaopay;
 import java.util.*;
 
 import com.dacoach.kakaopay.KakaoApproveResponse;
+import com.dacoach.kakaopay.KakaoCancelResponse;
+import com.dacoach.kakaopay.PayDTO;
 import com.dacoach.kakaopay.PayStatusDTO;
 public interface KakaopayMapper {
 
@@ -13,4 +15,8 @@ public interface KakaopayMapper {
 	public int upPayStatus(Map<String,String> map) throws Exception;
 	
 	public int insertPay(KakaoApproveResponse kar) throws Exception;
+	
+	public PayDTO paySelect(int pay_idx) throws Exception; 
+	
+	public int cancelOk(KakaoCancelResponse kcr) throws Exception;
 }
