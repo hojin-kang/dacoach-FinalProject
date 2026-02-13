b911e4fba7b2a3f94e11398b7997ff4c2bbf56b7
wonjae <staregreg01@naver.com>
2026-02-13 14:20:29 +0900

    adMinAd

---
+++ dacoach/src/main/java/com/dacoach/adminad/service/AdminAdManagementServiceImple.java
@@ -0,0 +1,48 @@
+package com.dacoach.adminad.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dacoach.mapper.adminAd.AdminAdManagementMapper;
+import com.dacoach.model.notification.NotificationDTO;
+import com.dacoach.service.notification.NotificationService;
+
+@Service
+public class AdminAdManagementServiceImple implements AdminAdManagementService{
+
+	@Autowired
+	AdminAdManagementMapper admapper;
+	
+	@Autowired 
+	NotificationService notificationService;
+
+	@Override
+	public List<Map<String, Object>> getAdRequestList() {
+		return admapper.getAdRequestList();
+	}
+
+	@Override	
+	public int sendNotification(int ad_idx, int user_idx,String action) {
+		NotificationDTO dto = new NotificationDTO();
+		dto.setProvider_idx(1);
+		dto.setReceiver_idx(user_idx);
+		dto.setNoti_type("AD");
+		int result = 0;
+		if("APPROVE".equals(action)) {
+			dto.setContent("신청하신 광고가 승인되었습니다. 알림 창에서 확인 가능합니다.");
+			result = notificationService.insertNotification(dto);
+		}else if("REJECT".equals(action)) {
+			dto.setContent("신청하신 광고가 반려되었습니다. 알림 창에서 확인 후 1:1 문의 부탁드립니다.");
+			notificationService.insertNotification(dto);
+			result = admapper.deleteAdRequest(ad_idx);
+		}
+		//System.out.println("isread=" +dto.getIs_read());
+		
+		return result;
+	}
+	
+	
+}
