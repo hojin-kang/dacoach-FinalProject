69a5c7bb706b56fc41f190902e75c6352aadb941
uoehoswi3 <thdus1821@naver.com>
2026-02-20 10:26:14 +0900

    채팅 빨간점표시 추가

--- dacoach/src/main/java/com/dacoach/controller/HeaderController.java
+++ dacoach/src/main/java/com/dacoach/controller/HeaderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.dacoach.service.chat.ChatService;
 import com.dacoach.service.notification.NotificationService;
 
 import jakarta.servlet.http.HttpSession;
@@ -13,11 +14,21 @@ public class HeaderController {
 
 	@Autowired
     private NotificationService notificationService;
+	
+	@Autowired
+	private ChatService chatService;
 
     @ModelAttribute("unreadNotiCount")
     public int unreadNotiCount(HttpSession session) {
         Integer usersIdx = (Integer) session.getAttribute("user_idx");
         if (usersIdx == null) return 0;
         return notificationService.countUnread(usersIdx);
+    }
+    
+    @ModelAttribute("unreadChatCount")
+    public int unreadChat(HttpSession session) {
+        Integer usersIdx = (Integer) session.getAttribute("user_idx");
+        if (usersIdx == null) return 0;
+        return chatService.countUnread(usersIdx);
     }
 }
